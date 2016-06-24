f4346752880e82d3c26f02b7592ef6a2c872079b
Christian Danowski <c.danowski@52north.org>
2016-06-24 11:20:58 +0200

    add basic i18n support

--- app/app.js
+++ app/app.js
@@ -1,2 +1,22 @@
 // Declare app level module which depends on views, and components
-angular.module('wpsClient', [ 'wpsUserInterface' ]);
+var appModule = angular.module('wpsClient', [ 'pascalprecht.translate',
+		'wpsUserInterface' ]);
+
+appModule
+		.config([
+				'$translateProvider',
+				function($translateProvider) {
+					$translateProvider.useStaticFilesLoader({
+						prefix : 'i18n/',
+						suffix : '.json'
+					});
+					var suppLang = [ 'en', 'de' ];
+					$translateProvider.registerAvailableLanguageKeys(suppLang);
+					$translateProvider.determinePreferredLanguage();
+					if ($translateProvider.preferredLanguage() === ''
+							|| suppLang.indexOf($translateProvider
+									.preferredLanguage()) === -1) {
+						$translateProvider.preferredLanguage('en');
+					}
+					$translateProvider.useSanitizeValueStrategy('escape');
+				} ]);
