db224a8942183ab6cfca6c079653c7cfcbe5155e
Christian Danowski <christian.danowski@hs-bochum.de>
2016-06-29 10:23:47 +0200

    provide OL map via a service, instead of directly in controller

--- app/components/wpsMap/wps-map.component.js
+++ app/components/wpsMap/wps-map.component.js
@@ -2,32 +2,13 @@ angular.module('wpsMap').component(
 		'wpsMap',
 		{
 			templateUrl : "components/wpsMap/wps-map.template.html",
-			controller : function MapController() {
+			controller : ['wpsMapService', function MapController(wpsMapService) {
 
-				this.map;
+				this.wpsMapService = wpsMapService;
+				
+				this.initializeMap = function(){
+					this.wpsMapService.initializeMap();
+				};
 
-				this.initializeMap = function() {
-
-					/*
-					 * TODO better use a service that handles map creation, interaction etc!!!
-					 */
-					
-					var map = new ol.Map({
-						target : 'map',
-						layers : [ new ol.layer.Tile({
-							source : new ol.source.MapQuest({
-								layer : 'osm'
-							})
-						}), ],
-						view : new ol.View({
-							center : ol.proj.transform([ 7.63, 51.95 ],
-									'EPSG:4326', 'EPSG:3857'),
-							zoom : 14
-						}),
-						controls : ol.control.defaults().extend(
-								[ new ol.control.ScaleLine() ])
-					});
-				}
-
-			}
+			}]
 		});
--- app/components/wpsMap/wps-map.module.js
+++ app/components/wpsMap/wps-map.module.js
@@ -1,1 +1,32 @@
 angular.module('wpsMap', []);
+
+/**
+ * a common serviceInstance that holds all needed properties and methods for
+ * interacting with a map (openlayers).
+ */
+angular.module('wpsMap').service(
+		'wpsMapService',
+		function() {
+
+			this.map;
+
+			this.initializeMap = function() {
+
+				this.map = new ol.Map({
+					target : 'map',
+					layers : [ new ol.layer.Tile({
+						source : new ol.source.MapQuest({
+							layer : 'osm'
+						})
+					}), ],
+					view : new ol.View({
+						center : ol.proj.transform([ 7.63, 51.95 ],
+								'EPSG:4326', 'EPSG:3857'),
+						zoom : 14
+					}),
+					controls : ol.control.defaults().extend(
+							[ new ol.control.ScaleLine() ])
+				});
+			}
+
+		});
