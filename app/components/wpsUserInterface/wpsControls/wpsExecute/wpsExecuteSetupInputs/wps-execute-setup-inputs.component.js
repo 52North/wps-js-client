75d7b77b8d324b307c19cbf8cda3f8e278e7b30d
Christian Danowski <c.danowski@52north.org>
2016-07-15 09:24:22 +0200

    handle click on already defined input
    now shows the corresponding input from and fills its values with the
    already set properties of the defined/selected input

--- app/components/wpsUserInterface/wpsControls/wpsExecute/wpsExecuteSetupInputs/wps-execute-setup-inputs.component.js
+++ app/components/wpsUserInterface/wpsControls/wpsExecute/wpsExecuteSetupInputs/wps-execute-setup-inputs.component.js
@@ -74,6 +74,100 @@ angular
 									this.wpsExecuteInputServiceInstance.asReference = false;
 									this.wpsExecuteInputServiceInstance.complexPayload = undefined;
 								};
+								
+								this.onChangeAlreadyDefinedExecuteInput = function(configuredInput){
+									/*
+									 * user selected an already defined input
+									 * 
+									 * now identify it, show the corresponding form 
+									 * and fill the form elements with the defined values!
+									 */
+									var selectedInput = this.wpsExecuteInputServiceInstance.selectedExecuteInput;
+									
+									var definedInput = this.getDefinedInput(selectedInput, this.wpsPropertiesServiceInstance.executeRequest.inputs);
+									
+									/*
+									 * depending on the type of the definedInput 
+									 * we have to fill in a different form
+									 * 
+									 * type may be "literal", "complex", "bbox" 
+									 * according to InputGenerator-class from wps-js-lib library
+									 */
+									var type = definedInput.type;
+									
+									switch (type) {
+									
+									case "literal":
+										this.fillLiteralInputForm(definedInput);
+										break;
+									
+									case "complex":
+										this.fillComplexInputForm(definedInput);
+										break;
+										
+									case "bbox":
+										this.fillBoundingBoxInputForm(definedInput);
+									
+									}
+								};
+								
+								
+								this.fillLiteralInputForm = function(literalInput){
+									this.wpsExecuteInputServiceInstance.literalInputValue = literalInput.value;
+								};
+								
+								this.fillBoundingBoxInputForm = function(bboxInput){
+									/*
+									 * TODO
+									 */
+								};
+								
+								this.fillComplexInputForm = function(complexInput){
+
+									this.wpsExecuteInputServiceInstance.asReference = complexInput.asReference;
+									
+									this.wpsExecuteInputServiceInstance.complexPayload = complexInput.complexPayload;
+									
+									this.wpsExecuteInputServiceInstance.selectedExecuteInputFormat = this.getSelectedExecuteInputFormatcomplexInput(complexInput.mimeType, this.wpsExecuteInputServiceInstance.selectedExecuteInput.complexData.formats);
+									
+								};
+								
+								this.getSelectedExecuteInputFormatcomplexInput = function(mimeType, formatsList){
+									var index;
+									
+									for(var i=0; i<formatsList.length; i++){
+										var currentFormat = formatsList[i];
+										
+										/*
+										 * some element must have the same identifier
+										 */
+										if(mimeType === currentFormat.mimeType){
+											index = i;
+											break;
+										}		
+									}
+									
+									return formatsList[index];
+								};
+								
+								this.getDefinedInput = function(selectedInput, definedInputsList){
+									var id = selectedInput.identifier;
+									var index;
+									
+									for(var i=0; i<definedInputsList.length; i++){
+										var currentDefinedInput = definedInputsList[i];
+										
+										/*
+										 * some element must have the same identifier
+										 */
+										if(id === currentDefinedInput.identifier){
+											index = i;
+											break;
+										}		
+									}
+									
+									return definedInputsList[index];
+								};
 
 							} ]
 				});
