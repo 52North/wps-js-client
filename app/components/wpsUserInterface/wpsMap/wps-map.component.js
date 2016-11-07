ec0763f721a3a46ab37e4acfe65ceaf86810cd1b
MojioMS <Maurin.Radtke@uni-muenster.de>
2016-11-07 15:07:55 +0100

    leaflet-directive for angular and leaflet draw

--- app/components/wpsUserInterface/wpsMap/wps-map.component.js
+++ app/components/wpsUserInterface/wpsMap/wps-map.component.js
@@ -1,18 +1,56 @@
 angular.module('wpsMap').component(
-		'wpsMap',
-		{
-			templateUrl : "components/wpsUserInterface/wpsMap/wps-map.template.html",
-			controller : ['wpsMapService', function MapController(wpsMapService) {
+        'wpsMap',
+        {
+            templateUrl: "components/wpsUserInterface/wpsMap/wps-map.template.html",
+            controller: ['$scope',
+                '$timeout',
+                'wpsMapService',
+                'leafletData',
+                function MapController($scope, $timeout, wpsMapService, leafletData) {
 
-				this.wpsMapService = wpsMapService;
-				
-				this.initializeMap = function(){
-					this.wpsMapService.initializeMap();
-				};
-        
-        // here include other methods for map interaction 
-        // (like drawing or selecting geometries or enable/disable map editing)
-        // within those methods call the associated method from "wpsMapService"
+                    angular.extend($scope, {
+                        center: {
+                            lat: 51.95,
+                            lng: 7.63,
+                            zoom: 13
+                        },
+                        controls: {
+                            draw: {}
+                        }
+                    });
 
-			}]
-		});
+                    leafletData.getMap().then(function (map) {
+                        var drawnItems = new L.featureGroup().addTo(map);
+
+                        map.on('draw:created', function (e) {
+                            var layer = e.layer;
+                            drawnItems.addLayer(layer);
+                            console.log(JSON.stringify(layer.toGeoJSON()));
+                        });
+
+                    });
+
+                    $scope.drawItems = {
+                        polyline: false,
+                        polygon: false,
+                        circle: false,
+                        rectangle: {
+                            metric: false,
+                            showArea: true,
+                            shapeOptions: {
+                                color: "#1A80C1"
+                            }
+                        },
+                        marker: false
+                    };
+
+                    //this.wpsMapService = wpsMapService;
+
+
+
+                    // here include other methods for map interaction 
+                    // (like drawing or selecting geometries or enable/disable map editing)
+                    // within those methods call the associated method from "wpsMapService"
+
+                }]
+        });
--- app/components/wpsUserInterface/wpsMap/wps-map.module.js
+++ app/components/wpsUserInterface/wpsMap/wps-map.module.js
@@ -1,29 +1,41 @@
 angular.module('wpsMap', []);
-
 /**
  * a common serviceInstance that holds all needed properties and methods for
  * interacting with a map (openlayers).
  */
 angular.module('wpsMap').service(
-		'wpsMapService',
-		function() {
+        'wpsMapService',
+        function () {
+            /**
+            // central map object
+            this.map;
+            
+            
+                        
+            this.drawnItems;
+            this.initializeMap = function () {
 
-      // central map object
-			this.map;
+                // initialize map referring to div element with id="map"
+                this.map = L.map('map', {drawControl: true}).setView([51.95, 7.63], 13);
+                
+                // create OSM tile layer with correct attribution
+                var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+                var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+                var osm = new L.TileLayer(osmUrl, {minZoom: 1, maxZoom: 19, attribution: osmAttrib}).addTo(this.map);
+                // Initialise the FeatureGroup to store editable layers
+                this.drawnItems = new L.FeatureGroup();
+                this.map.addLayer(this.drawnItems);
+                // Initialise the draw control and pass it the FeatureGroup of editable layers
+                var drawControl = new L.Control.Draw({
+                    edit: {
+                        featureGroup: this.drawnItems
+                    }
+                });
+                this.map.addControl(drawControl);
+                
+            };*/
+            
+            // here include other methods for map interaction 
+            // (like drawing or selecting geometries or enable/disable map editing)
 
-			this.initializeMap = function() {
-
-        // initialize map referring to div element with id="map"
-        this.map = L.map('map').setView([51.95, 7.63], 13);
-      
-        // create OSM tile layer with correct attribution
-        var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-        var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-        var osm = new L.TileLayer(osmUrl, {minZoom: 1, maxZoom: 19, attribution: osmAttrib}).addTo(this.map);		
-      
-			}
-      
-      // here include other methods for map interaction 
-      // (like drawing or selecting geometries or enable/disable map editing)
-
-		});
+        });
